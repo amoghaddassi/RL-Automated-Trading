9e83429e1abd2a6a0a5c61a445f99557df162fe9
Aram Moghaddassi <moghadddassi.aram@gmail.com>
2018-11-07 17:01:03 -0800

    added the training loop.

--- environment.py
+++ environment.py
@@ -1,6 +1,8 @@
+#Environment switches
 START_CASH = 1000
 
 class Environment:
+    """Represents the trading environment for an asset with price history data."""
     def __init__(self, data):
         self.data = data
         self.index = 0
@@ -8,43 +10,46 @@ class Environment:
         self.cash = START_CASH
 
     def state(self):
-        """Holds price of all assets that we're trading"""
-        price = self.data.iloc[self.index]['Open']
-        return price
+        """Returns the price of the asset at the current index"""
+        return self.data.iloc[self.index]['Open']
 
     def step(self, action):
-        """Takes a given action in the env, and returns:
-        1. next_state: the price of all assets in state at time t+1
+        """Takes a given action (action should be some integer) in the env, and returns:
+        1. next_state: the price of the asset at time t+1
         2. reward: reward associated with the action"""
         self.index += 1
         next_state = self.state()
 
         past_balance = self.account_value()
         self.place_order(action)
-        reward = self.eval_state(past_balance)
+        reward = self.reward(past_balance)
 
         return next_state, reward
 
-    def place_order(self, action):
-        if action > 0: #buy order
+    def place_order(self, amount):
+        """Executes a trade that buys amount shares if amount > 0,
+        and sells -amount shares otherwise."""
+        amount = int(amount)
+        if amount > 0: #buy order
             price = self.state()
-            cost = price * action
+            cost = price * amount
             if cost > self.cash:
                 return #invalid trade
             self.cash -= cost
-            self.position += action
-        elif action < 0: #sell order
-            if action > self.position: #invalid trade
+            self.position += amount
+        elif amount < 0: #sell order
+            if amount > self.position: #invalid trade
                 return
             price = self.state()
-            proceeds = price * action
+            proceeds = price * amount
             self.cash += proceeds
-            self.position += action
+            self.position += amount
 
     def account_value(self):
+        """Returns the combined value of cash and stock in the account."""
         return self.cash + self.position * self.state()
 
-    def eval_state(self, past_balance):
+    def reward(self, past_balance):
         """Takes a past balance and evaluates how well the most recent action impacted the balance."""
         return self.account_value() - past_balance #very simple measure, need to update.
 
--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 from environment import Environment
+from pybrain3.structure.modules.gaussianlayer import GaussianLayer
 from pybrain3.rl.agents.learning import LearningAgent
 from pybrain3.rl.learners.valuebased.nfq import NFQ
 import pandas as pd
@@ -7,6 +8,25 @@ path = '/Users/arammoghaddassi/Google Drive/Projects/RL-Automated-Trading/data/'
 aapl = pd.read_csv(path + 'AAPL.csv')
 amzn = pd.read_csv(path + 'AMZN.csv')
 
-n_trials = 1000 #Length of training loop.
+#Model switches
+n_episodes = 10
+episdoe_length = 30
 
-env, agent = Environment(aapl), LearningAgent(NFQ)
+gaussian_layer = GaussianLayer(2)
+nfq_learner = NFQ()
+
+env, agent = Environment(aapl), LearningAgent(gaussian_layer, learner=nfq_learner)
+
+agent.reset(), env.reset()
+
+for ep in range(n_episodes):
+    agent.newEpisode()
+    for i in range(episdoe_length):
+        state = env.state()
+        agent.integrateObservation(state)
+        action = agent.getAction() #Causing an error right now.
+        state, reward = env.step(action)
+        agent.giveReward(reward)
+        print("Episode: {}, Trial: {}, Balance: {}".format(ep, i, env.account_value()))
+        agent.learn() #When/how should I actually call this method?
+
